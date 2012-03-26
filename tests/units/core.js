e045fa553a13e6d59766a79e6982c442287fa813
Nikolas Leßmann <nlessmann@buero-fuer-ideen.de>
2012-03-26 22:42:25 +0200

    More tests added

--- tests/units/core.js
+++ tests/units/core.js
@@ -19,7 +19,7 @@ test('Instantiation', function() {
 });
 
 test('Registration and removal of routes', function() {
-    expect(6);
+    expect(11);
     
     var s = new Simrou();
     
@@ -28,13 +28,22 @@ test('Registration and removal of routes', function() {
     equal(typeof r, 'object', 'RegisterRoute() returns an object.');
     equal(String(r.getRegExp()), '/^.+$/', 'Registering a new route without passing any arguments returns a wildcard route.');
     
+    // Move everything to "route.js" ... test only registration and removal at this point!
+    
     var m = r.match('asf');
     ok($.isArray(m), 'match("asf") returns an array for this route.');
-    equal(m.length, 0, '..and that array is empty.');
+    equal(m.length, 0, '...and that array is empty.');
     strictEqual(r.match(''), false, 'match("") === false');
     
-    // Register a route and specify only the pattern (static)
+    // Register a route and specify a static the pattern only
     r = s.registerRoute('asf');
+    equal(String(r.getRegExp()), String(/^asf$/), 'Registering a route with a static pattern leads to the correct regular expression.');
+    
+    var m = r.match('asf');
+    ok($.isArray(m), 'match("asf") return an array for this route.');
+    equal(m.length, 0, '...and that array is empty.');
+    strictEqual(r.match(''), false, 'match("") === false');
+    strictEqual(r.match('qwe'), false, 'match("qwe") === false');
     
     // Register a route with mixed types of parameters in the pattern
     r = s.registerRoute('/test/:named/*splat/:anotherNamed/*');
