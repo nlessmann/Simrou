04e430603c2125e481b09abd05e893e8e26ba130
Nikolas Leßmann <nlessmann@buero-fuer-ideen.de>
2012-03-27 14:26:49 +0200

    Minor updates on test for route.assemble()

--- tests/units/route.js
+++ tests/units/route.js
@@ -30,20 +30,20 @@ test('Assembling a route', function() {
     var r = s.registerRoute('/test/:named/*splat/:anotherNamed/*');
     
     // Test normal assembling
-    var a = r.assemble('0', 'john', 'doe', 'junior');
+    var a = r.assemble(0, 'john', 'doe', 'junior');
     equal(a, '/test/0/john/doe/junior', 'Assembling a route with values for all parameters works.');
     
     // Miss out the last two parametes
-    a = r.assemble('0', 'john');
-    equal(a, '/test/0/john//', 'Assembling a route with values for only some of the parameters works.');
+    a = r.assemble('mr', 'john');
+    equal(a, '/test/mr/john//', 'Assembling a route with values for only some of the parameters works.');
     
     // Hand over an array instead of single parameters
-    a = r.assemble(['0', 'john', 'doe', 'junior']);
-    equal(a, '/test/0/john/doe/junior', 'Providing the values in an array works.');
+    a = r.assemble(['mr', 'john', 'doe', 'junior']);
+    equal(a, '/test/mr/john/doe/junior', 'Providing the values in an array works.');
     
     // Hand over functions
-    a = r.assemble('0', function() { return 'john'; }, 'doe', 'junior');
-    equal(a, '/test/0/john/doe/junior', 'Providing a function for a parameter works.');
+    a = r.assemble('mr', function() { return 'john'; }, 'doe', 'junior');
+    equal(a, '/test/mr/john/doe/junior', 'Providing a function for a parameter works.');
     
     // Try to assemble a route based on a regular expression
     r = s.registerRoute(/^.+$/);
