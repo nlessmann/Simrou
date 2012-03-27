eac732ddfbbc3dee84928f1a4ed7ea212ec3b74b
Nikolas Leßmann <nlessmann@buero-fuer-ideen.de>
2012-03-27 13:52:03 +0200

    Test for route.assemble() finished

--- tests/units/route.js
+++ tests/units/route.js
@@ -20,3 +20,26 @@ module('Route');
         'Registering a route containing mixed parameter types returns the expected regular expression.');
         
 */
+
+test('Assembling a route', function() {
+    expect(4);
+    
+    var s = new Simrou();
+    var r = s.registerRoute('/test/:named/*splat/:anotherNamed/*');
+    
+    // Test normal assembling
+    var a = r.assemble('mr', 'john', 'doe', 'junior');
+    equal(a, '/test/mr/john/doe/junior', 'Assembling a route with values for all parameters works.');
+    
+    // Miss out the last two parametes
+    a = r.assemble('mr', 'john');
+    equal(a, '/test/mr/john//', 'Assembling a route with values for only some of the parameters works.');
+    
+    // Hand over an array instead of single parameters
+    a = r.assemble(['mr', 'john', 'doe', 'junior']);
+    equal(a, '/test/mr/john/doe/junior', 'Providing the values in an array works.');
+    
+    // Hand over functions
+    a = r.assemble('mr', function() { return 'john'; }, 'doe', 'junior');
+    equal(a, '/test/mr/john/doe/junior', 'Providing a function for a parameter works.');
+});
